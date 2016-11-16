d79693c2ff83075e2a9bdac9fa0e84c6e13e9fb2
Marco Doni <zerouno@gmail.com>
2016-11-16 16:01:27 +0100

    Fix not implemented abstract InputTypes using general type TextType

--- src/main/java/it/cineca/iris/ir/rest/model/utils/InputTypeEnum.java
+++ src/main/java/it/cineca/iris/ir/rest/model/utils/InputTypeEnum.java
@@ -6,23 +6,23 @@ public enum InputTypeEnum {
 	 * the int constant for null InputTypeEnum
 	 */
 	textarea("textarea", TextType.class), 
-	name("name", AbstractInputformType.class), 
-	date("date", AbstractInputformType.class), 
-	dropdown("dropdown", AbstractInputformType.class),
-	qualdrop_value("qualdrop_value", AbstractInputformType.class),
-	qualdrop_text("qualdrop_text", AbstractInputformType.class), 
-	controlled_name("controlled_name", AbstractInputformType.class), 
-	default_value("default_value", AbstractInputformType.class),
+	name("name", TextType.class), 
+	date("date", TextType.class), 
+	dropdown("dropdown", TextType.class),
+	qualdrop_value("qualdrop_value", TextType.class),
+	qualdrop_text("qualdrop_text", TextType.class), 
+	controlled_name("controlled_name", TextType.class), 
+	default_value("default_value", TextType.class),
 	sharepriority("sharepriority", TextType.class),
-	onebox("onebox", AbstractInputformType.class),
-	twobox("twobox", AbstractInputformType.class),
-	list("list", AbstractInputformType.class),
-	ance("ance", AbstractInputformType.class),
-	fulldate("fulldate", AbstractInputformType.class),
+	onebox("onebox", TextType.class),
+	twobox("twobox", TextType.class),
+	list("list", TextType.class),
+	ance("ance", TextType.class),
+	fulldate("fulldate", TextType.class),
 	year("year",YearType.class),
-	checkbox("checkbox", AbstractInputformType.class),
-	annotation("annotation", AbstractInputformType.class),
-	dropdown_json("dropdown_json", AbstractInputformType.class);
+	checkbox("checkbox", TextType.class),
+	annotation("annotation", TextType.class),
+	dropdown_json("dropdown_json", TextType.class);
 	
 	private String keyEnum;
 	
